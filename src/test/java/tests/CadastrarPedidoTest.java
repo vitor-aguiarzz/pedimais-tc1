f1db9d50b07e145be3a4ec829b38d350862fa16d
brenoaissa <brenoaugusto05@hotmail.com.br>
2025-06-15 20:27:44 -0300

    test: adicionar teste para telefone inválido com mais de 11 dígitos
    Confirma que telefones com número excessivo de dígitos são rejeitados.

--- src/test/java/tests/CadastrarPedidoTest.java
+++ src/test/java/tests/CadastrarPedidoTest.java
@@ -169,4 +169,22 @@ public class CadastrarPedidoTest {
         assertNotEquals("Pedido cadastrado com sucesso!", alert.getText());
     }
 
+    @Test
+    @DisplayName("Não deve cadastrar pedido quando telefone contém mais de 11 dígitos")
+    public void naoDeveCadastrarPedidoQuandoTelefoneMaisDeOnzeDigitos() {
+        cadastrarPedidoPage.preencherNome(faker.name().fullName());
+        cadastrarPedidoPage.preencherCpf("16493614082");
+        cadastrarPedidoPage.preencherTelefone("1234567890987654321");
+        cadastrarPedidoPage.preencherEmail(faker.internet().emailAddress());
+        cadastrarPedidoPage.preencherEndereco(faker.address().streetAddress());
+        cadastrarPedidoPage.clicaComboBoxPizza();
+        cadastrarPedidoPage.selecionaPizzaPeperoni();
+        cadastrarPedidoPage.cadastrarPedido();
+
+        Alert alert = new WebDriverWait(driver, Duration.ofSeconds(5))
+                .until(ExpectedConditions.alertIsPresent());
+        assertNotEquals("Pedido cadastrado com sucesso!", alert.getText());
+    }
+
+
 }
